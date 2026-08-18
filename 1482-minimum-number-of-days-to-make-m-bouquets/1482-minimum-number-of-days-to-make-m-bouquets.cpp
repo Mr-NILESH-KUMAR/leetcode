298b0dbd7c5a553d1510ec8f7eed13eab9bace01
Nilesh Kumar <btech10054.24@bitmesra.ac.in>
2026-08-19 01:02:49 +0530

    Time: 22 ms (56.87%), Space: 70.6 MB (48.88%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        if(bloomDay.size() < (long long)m*k) return -1;
-        int maxi = *max_element(bloomDay.begin(), bloomDay.end());
-        int l = 0; int r = maxi;
-        int ans = INT_MAX;
-        while(l <= r){
-            int mid = (l + r) >> 1;
-            int cntk = k;int temp = 0;int cntm = m;
-            for(int i = 0; i < bloomDay.size(); i++) {
-                if(bloomDay[i] <= mid) {
-                    cntk--;
-                    if(cntk == 0 && cntm != 0) {
-                        cntm--;
-                        cntk = k;
-                    }
-                    if(cntm == 0){
-                        temp = min(ans, mid);
-                        break;
-                    }
+        if(bloomDay.size()<(long long)m*k)return -1;
+        int lf = *min_element(bloomDay.begin(),bloomDay.end());
+        int rt = *max_element(bloomDay.begin(),bloomDay.end());
+        int ans =INT_MAX;
+        while(lf<=rt){
+            int mid = lf +(rt-lf)/2;
+            int cont=0,M=m;
+            for(int i=0;i<bloomDay.size();i++){
+                if(bloomDay[i]<=mid){
+                  cont++;                                       
                 }
                 else{
-                    cntk = k;
+                    M-=(cont/k);
+                    cont=0;
+
                 }
             }
-            if(temp != 0) {
-                r = mid - 1;
-                ans = temp;
-            }
-            else {
-                l = mid + 1;
-            }
-
+            M -= cont / k;
+                if(M<=0){
+                    ans = min(ans,mid);
+                    rt=mid-1;
+                }
+                else lf=mid+1;
         }
         return ans;
     }
